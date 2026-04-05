b616f40daf04e17996c10320d36d89f860e553f9
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2026-04-05 22:54:59 +0530

    NEW project springai.openai

---
+++ springai.openai/src/main/java/com/springai/openai/entity/Tut.java
@@ -0,0 +1,50 @@
+package com.springai.openai.entity;
+
+public class Tut {
+
+    private String title;
+    private String content;
+    private String createdYear;
+
+    public Tut(String title, String content, String createdYear) {
+        this.title = title;
+        this.content = content;
+        this.createdYear = createdYear;
+    }
+
+    public Tut() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getCreatedYear() {
+        return createdYear;
+    }
+
+    public void setCreatedYear(String createdYear) {
+        this.createdYear = createdYear;
+    }
+
+    @Override
+    public String toString() {
+        return "Tut{" +
+                "title='" + title + '\'' +
+                ", content='" + content + '\'' +
+                ", createdYear='" + createdYear + '\'' +
+                '}';
+    }
+}
